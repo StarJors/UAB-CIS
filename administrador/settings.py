fac435d0dd27ac4f00117b3401cf35856a37dddb
StarJors <joseriverosuarez95@gmail.com>
2024-09-04 18:06:11 -0400

    diploy

--- administrador/settings.py
+++ administrador/settings.py
@@ -98,9 +98,9 @@ DATABASES = {
     }
 }
 
-POSTGRES_LOCALLY = True
-if ENVIRONMENT == 'production' or POSTGRES_LOCALLY == True:
-    DATABASES['default'] = dj_database_url.parse(env('DATABASE_URL'))
+#POSTGRES_LOCALLY = True
+#if ENVIRONMENT == 'production' or POSTGRES_LOCALLY == True:
+#DATABASES['default'] = dj_database_url.parse(env('DATABASE_URL'))
 
 
 # Password validation
